891548ef333b9410ba257e3d0d97f98b2d459ed9
大超 <generalcaho@icloud.com>
2019-09-30 13:11:19 +0800

    Initial commit

---
+++ initLeanCloud.js
@@ -0,0 +1,8 @@
+// LeanCloud - 初始化 - 将这里的 APP_ID 和 APP_KEY 替换成自己的应用数据
+// https://leancloud.cn/docs/sdk_setup-js.html#初始化
+var APP_ID = 'A2aR83iQY7pvynjoKTNSafiA-gzGzoHsz';
+var APP_KEY = 'VU8qOgtiRR9lng21YGV98aPv';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
